0ffafde535f126e7e0c489077982c73057d62087
Zisis <zis.tax@gmail.com>
2013-10-03 00:31:50 +0300

    Moved shaders into text files. Cleared code. Renamed main class to Dungeon

---
+++ map/Dungeon.cpp
@@ -0,0 +1,110 @@
+/*
+ * Copyright © 2012-2013 Graham Sellers
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#include <sb6.h>
+#include <shader.h>
+#include <vmath.h>
+#include "Level.h"
+
+class singlepoint_app : public sb6::application
+{
+    void init()
+    {
+        static const char title[] = "OpenGL SuperBible - Spinny Cube";
+
+        sb6::application::init();
+
+        memcpy(info.title, title, sizeof(title));
+    }
+
+    virtual void startup()
+    {        
+        program = glCreateProgram();
+ 
+        GLuint fs = sb6::shader::load("fragment_shader.fg", GL_FRAGMENT_SHADER);
+        GLuint vs = sb6::shader::load("vertex_shader.vs", GL_VERTEX_SHADER);
+        
+        glAttachShader(program, vs);
+        glAttachShader(program, fs);
+
+        glLinkProgram(program);
+
+        mv_location = glGetUniformLocation(program, "mv_matrix");
+        proj_location = glGetUniformLocation(program, "proj_matrix");
+
+        glGenVertexArrays(1, &vao);
+        glBindVertexArray(vao);
+     
+        glEnable(GL_CULL_FACE);
+        glFrontFace(GL_CCW);
+
+        glEnable(GL_DEPTH_TEST);
+        glDepthFunc(GL_LEQUAL);
+
+		level = new Level(10, 6);
+    }
+
+    virtual void render(double currentTime)
+    {
+        static const GLfloat green[] = { 0.0f, 0.25f, 0.0f, 1.0f };
+        static const GLfloat one = 1.0f;
+
+        glViewport(0, 0, info.windowWidth, info.windowHeight);
+        glClearBufferfv(GL_COLOR, 0, green);
+        glClearBufferfv(GL_DEPTH, 0, &one);
+
+        glUseProgram(program);
+
+        glUniformMatrix4fv(proj_location, 1, GL_FALSE, proj_matrix);
+
+		level->draw(program);
+	}
+
+    virtual void shutdown()
+    {
+        glDeleteVertexArrays(1, &vao);
+        glDeleteProgram(program);
+		delete level;
+    }
+
+    void onResize(int w, int h)
+    {
+        sb6::application::onResize(w, h);
+
+        aspect = (float)w / (float)h;
+        proj_matrix = vmath::perspective(50.0f, aspect, 0.1f, 1000.0f);
+    }
+
+private:
+    GLuint          program;
+    GLuint          vao;
+
+    GLint           mv_location;
+    GLint           proj_location;
+
+    float           aspect;
+    vmath::mat4     proj_matrix;
+	IDrawable*		level;
+};
+
+DECLARE_MAIN(singlepoint_app)
---
+++ map/vertex_shader.vs
@@ -0,0 +1,19 @@
+#version 420 core                                                  
+                                                                   
+layout (location = 0) in vec4 position;                            
+layout (location = 1) in vec4 color;                               
+                                                                   
+out VS_OUT                                                         
+{                                                                  
+    vec4 color;                                                    
+} vs_out;                                                          
+                                                                   
+uniform mat4 mv_matrix;                                            
+uniform mat4 proj_matrix;                                          
+                                                                   
+void main(void)                                                    
+{                                                                  
+    gl_Position = proj_matrix * mv_matrix * position;              
+  //vs_out.color = position * 2.0 + vec4(0.5, 0.5, 0.5, 0.0);					
+    vs_out.color = color;													    
+}                                                                  
